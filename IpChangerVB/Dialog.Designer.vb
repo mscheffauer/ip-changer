7d9fe810a11fd57d29097111d8acb6a593e769e0
Martin Scheffauer <59596935+mscheffauer@users.noreply.github.com>
2021-07-13 13:23:41 +0200

    Add files via upload

---
+++ IpChangerVB/Dialog.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Dialog
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.LabelSaved = New System.Windows.Forms.Label()
+        Me.TimerSaved = New System.Windows.Forms.Timer(Me.components)
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(167, 259)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 61)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select Set"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.Location = New System.Drawing.Point(29, 49)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(200, 204)
+        Me.ListBox1.TabIndex = 37
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 17)
+        Me.Label1.TabIndex = 38
+        Me.Label1.Text = "Ip-Address"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(230, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 17)
+        Me.Label2.TabIndex = 39
+        Me.Label2.Text = "Subnetmask"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(456, 125)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(83, 50)
+        Me.Button2.TabIndex = 40
+        Me.Button2.Text = "Edit Selected"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(456, 201)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(83, 50)
+        Me.Button3.TabIndex = 41
+        Me.Button3.Text = "Save all"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(456, 49)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(83, 50)
+        Me.Button4.TabIndex = 42
+        Me.Button4.Text = "Copy from current"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'LabelSaved
+        '
+        Me.LabelSaved.AutoSize = True
+        Me.LabelSaved.ForeColor = System.Drawing.Color.Green
+        Me.LabelSaved.Location = New System.Drawing.Point(488, 293)
+        Me.LabelSaved.Name = "LabelSaved"
+        Me.LabelSaved.Size = New System.Drawing.Size(51, 17)
+        Me.LabelSaved.TabIndex = 43
+        Me.LabelSaved.Text = "Saved!"
+        Me.LabelSaved.Visible = False
+        '
+        'TimerSaved
+        '
+        Me.TimerSaved.Interval = 3000
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 345)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(28, 28)
+        Me.Button5.TabIndex = 44
+        Me.Button5.Text = "?"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 351)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(378, 17)
+        Me.Label3.TabIndex = 45
+        Me.Label3.Text = "Double-click on list item or Select Set Button to select a set"
+        Me.Label3.Visible = False
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(545, 214)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(40, 37)
+        Me.Button6.TabIndex = 46
+        Me.Button6.Text = "..."
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 25
+        Me.ListBox2.Location = New System.Drawing.Point(229, 49)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(200, 204)
+        Me.ListBox2.TabIndex = 47
+        '
+        'Dialog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(599, 385)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.LabelSaved)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Dialog"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Select Set"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents LabelSaved As Label
+    Friend WithEvents TimerSaved As Timer
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ListBox2 As ListBox
+End Class
---
+++ IpChangerVB/Dialog.vb
@@ -0,0 +1,284 @@
+﻿Module Module2
+
+    Public LastSubnet As String
+    Public LastIPAdress As String
+    Public IpNewEdit(4) As String
+    Public SubnetNewEdit(4) As String
+    Public EditFinished As Boolean
+
+    Public SelectedItemInd As Integer
+    Public IpToEdit(4) As String
+    Public SubnetToEdit(4) As String
+    Public DoActualize As Boolean
+End Module
+
+Public Class Dialog
+
+
+    Dim filepathress As String = "C:\ProgramData\IpChanger\IpAdresses.txt"
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        If Not (ListBox1.SelectedIndex = -1) Then
+
+            SetIpAdress = Split(ListBox1.SelectedItem.ToString, ".", 4)
+            SetSubnet = Split(ListBox2.SelectedItem.ToString, ".", 4)
+
+            setcmdIp = True
+            Form1.Timer1.Enabled = 1
+            Me.Close()
+        End If
+
+
+
+    End Sub
+
+    Private Sub FormMyb2227_KeyDown(sender As Object, e As KeyEventArgs) Handles ListBox1.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            Me.Close()
+        End If
+    End Sub
+    Private Sub FormMyb2228_KeyDown(sender As Object, e As KeyEventArgs) Handles Button1.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            Me.Close()
+        End If
+    End Sub
+    Private Sub FormMyb2229_KeyDown(sender As Object, e As KeyEventArgs) Handles Button2.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            Me.Close()
+        End If
+    End Sub
+    Private Sub FormMyb2230_KeyDown(sender As Object, e As KeyEventArgs) Handles Button2.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            Me.Close()
+        End If
+    End Sub
+    Private Sub Dialog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Dim Arr(3) As String
+        Dim file As System.IO.StreamReader
+        Dim TempString As String
+
+
+        If filepathress.Length < 260 Then
+
+            If My.Computer.FileSystem.FileExists(filepathress) Then
+
+                file = My.Computer.FileSystem.OpenTextFileReader(filepathress)
+
+                For i = 0 To 5
+                    TempString = file.ReadLine()
+
+                    Arr = Split(TempString, ",")
+                    SavedIpAdresses(i) = Arr(0)
+                    SavedSubnets(i) = Arr(1)
+
+
+
+
+                    ListBox1.Items.Add(SavedIpAdresses(i))
+                    ListBox2.Items.Add(SavedSubnets(i))
+                Next
+
+                file.Close()
+            Else
+                For i = 0 To 5
+                    SavedIpAdresses(i) = "0.0.0.0"
+                    SavedSubnets(i) = "0.0.0.0"
+
+                    ListBox1.Items.Add(SavedIpAdresses(i))
+                    ListBox2.Items.Add(SavedSubnets(i))
+                Next
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.DoubleClick
+
+        If Not (ListBox1.SelectedIndex = -1) Then
+
+            SetIpAdress = Split(ListBox1.SelectedItem.ToString, ".", 4)
+            SetSubnet = Split(ListBox2.SelectedItem.ToString, ".", 4)
+
+            setcmdIp = True
+            Form1.Timer1.Enabled = True
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.DoubleClick
+
+        If Not (ListBox2.SelectedIndex = -1) Then
+
+            SetIpAdress = Split(ListBox1.SelectedItem.ToString, ".", 4)
+            SetSubnet = Split(ListBox2.SelectedItem.ToString, ".", 4)
+
+            setcmdIp = True
+            Form1.Timer1.Enabled = True
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+
+        Dim file As System.IO.StreamWriter
+
+        If filepathress.Length < 260 Then
+            If My.Computer.FileSystem.FileExists(filepathress) Then
+                My.Computer.FileSystem.DeleteFile(filepathress)
+
+            End If
+
+            file = My.Computer.FileSystem.OpenTextFileWriter(filepathress, True)
+            For i = 0 To 5
+                file.WriteLine(SavedIpAdresses(i) & "," & SavedSubnets(i))
+
+            Next
+            file.Close()
+            LabelSaved.Visible = True
+            TimerSaved.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim temparr(4) As String
+        Dim temparr2(8) As String
+
+
+
+
+        If Not (ListBox1.SelectedIndex = -1) Then
+            DialogEdit.Show()
+
+            IpToEdit = Split(ListBox1.SelectedItem.ToString, ".", 4)
+            SubnetToEdit = Split(ListBox2.SelectedItem.ToString, ".", 4)
+
+            DoActualize = 1
+            SelectedItemInd = ListBox1.SelectedIndex
+            DialogEdit.Timer1.Enabled = True
+        End If
+
+
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        Dim text As String
+        Dim text2 As String
+        Dim i As Integer
+
+
+        If FocusPopUp Then
+            FocusPopUp = False
+            ListBox1.SelectedIndex = 0
+            ListBox1.Select()
+            ListBox2.SelectedIndex = 0
+            ListBox2.Select()
+            Timer1.Enabled = False
+        End If
+
+        If EditFinished Then
+            EditFinished = False
+
+            text = ""
+            For i = 0 To 2
+                text = text & IpNewEdit(i) & "."
+            Next
+            text = text & IpNewEdit(3)
+            SavedIpAdresses(SelectedItemInd) = text
+
+            text2 = ""
+            For i = 0 To 2
+                text2 = text2 & SubnetNewEdit(i) & "."
+            Next
+            text2 = text2 & SubnetNewEdit(3)
+            SavedSubnets(SelectedItemInd) = text2
+
+
+            ListBox1.Items.Item(SelectedItemInd) = text
+            ListBox2.Items.Item(SelectedItemInd) = text2
+            Timer1.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SelectedItemInd = ListBox1.SelectedIndex
+
+
+        If LastIPAdress IsNot vbNullString And LastSubnet IsNot vbNullString Then
+
+            ListBox1.Items.Item(SelectedItemInd) = LastIPAdress
+            ListBox2.Items.Item(SelectedItemInd) = LastSubnet
+
+            SavedIpAdresses(SelectedItemInd) = LastIPAdress
+
+
+            SavedSubnets(SelectedItemInd) = LastSubnet
+
+
+        End If
+    End Sub
+
+    Private Sub TimerSaved_Tick(sender As Object, e As EventArgs) Handles TimerSaved.Tick
+        LabelSaved.Visible = False
+        TimerSaved.Enabled = False
+    End Sub
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Label3.Visible Then
+            Label3.Visible = False
+            Button5.BackColor = SystemColors.Control
+        Else
+            Label3.Visible = True
+            Button5.BackColor = Color.DarkGray
+
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Process.Start("explorer.exe", "C:\ProgramData\IpChanger\")
+    End Sub
+
+
+
+
+
+    Private Sub ListBox1_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles ListBox1.MouseClick
+
+
+        ListBox2.SelectedIndex = ListBox1.SelectedIndex
+
+
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles ListBox2.MouseClick
+
+
+
+        ListBox1.SelectedIndex = ListBox2.SelectedIndex
+
+
+
+    End Sub
+
+
+
+
+
+End Class
